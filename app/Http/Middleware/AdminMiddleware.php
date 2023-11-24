1a9718343cf405f5ff56eb499b57c6c9f992a616
Mochammad Ikhsan Nawawi <ikhsannawawi99@gmail.com>
2023-11-24 14:09:12 +0700

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   app/Http/Middleware/AdminMiddleware.php

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -32,18 +32,25 @@ class AdminMiddleware
     // }
 
     public function handle($request, Closure $next)
-{
-    if (auth()->guard('admin')->check() && $request->is('admin/login')) {
-        // Jika pengguna sudah memiliki sesi admin dan mencoba mengakses halaman login, maka arahkan ke dashboard
-        return redirect()->route('admin.dashboard')->with('error', 'Anda sudah memiliki sesi.');
-    }
+    {
+        // Check if the user is authenticated as an admin
+        if (auth()->guard('admin')->check()) {
+            $user = auth()->guard('admin')->user();
 
-    // Jika pengguna tidak memiliki sesi admin dan mencoba mengakses halaman selain login, arahkan ke halaman login
-    if (!auth()->guard('admin')->check() && !$request->is('admin/login')) {
-        return redirect()->route('admin.login')->with('error', 'Anda tidak memiliki sesi.');
-    }
+            // Check if the admin user's status is 1
+            if ($user->status == 1) {
+                // If the status is 1, allow access to the requested route
+                return $next($request);
+            } else {
+                // If the status is not 1, log out the user and redirect to the login page
+                auth()->guard('admin')->logout();
+                $request->session()->invalidate();
+                return redirect()->route('admin.login')->with('info', 'Akunmu sudah tidak aktif.');
+            }
+        }
 
-    return $next($request);
-}
+        // If the user is not authenticated as an admin, redirect to the login page
+        return redirect()->route('admin.login')->with('warning', 'Kamu belum login.');
+    }
 
 }
